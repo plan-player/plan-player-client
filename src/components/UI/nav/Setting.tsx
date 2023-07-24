d399b2f4c4ff07d1bf86ecc279c243e0747c28d3
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-07-24 14:43:03 +0900

    Fix: Nav / Set wrapper height based on setting size #15

--- src/components/UI/nav/Setting.tsx
+++ src/components/UI/nav/Setting.tsx
@@ -1,11 +1,14 @@
 import { FaGear } from 'react-icons/fa6';
 import { styled } from 'styled-components';
 
+const SETTING_PADDING = 2;
+const SETTING_TEXT = 1;
+export const SETTING_SIZE = SETTING_PADDING * 2 + SETTING_TEXT;
+
 const SettingWrapper = styled.div`
   svg {
-    margin-right: 1.875rem;
-    margin-top: 1.565rem;
-    margin-bottom: 5vh;
+    padding: ${SETTING_PADDING}rem;
+    font-size: ${SETTING_TEXT}rem;
   }
 `;
 
