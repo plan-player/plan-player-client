7629377f30efd2241d18f2c0d16f106679d0a265
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-08-09 14:22:25 +0900

    Refactor: Schedule / Use selector for getting schedule or reduced recodes #45

--- src/atoms/recordAtom.ts
+++ src/atoms/recordAtom.ts
@@ -1,4 +1,5 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
+import { timeSliderValueAtom } from './timeSliderAtom';
 
 export interface RecordType {
   id: number | string;
@@ -76,4 +77,47 @@ export const recordsAtom = atom<RecordType[]>({
       category_group_color: 'blue',
     },
   ],
+});
+
+export const scheduleSelector = selector({
+  key: 'getSchedules',
+  get: ({ get }) => {
+    return get(recordsAtom).filter((record) => !record.is_history);
+  },
+});
+
+// 현재 타임슬라이더 값에 따라 이전으로는 history, 이후로는 schedule을 반환하는 selector
+export const dividedRecordsSelector = selector({
+  key: 'getDividedRecords',
+  get: ({ get }) => {
+    const current = get(timeSliderValueAtom);
+    const records = get(recordsAtom);
+    return records
+      .reduce((acc, record) => {
+        const { start, end, is_history } = record;
+
+        // history의 end값이 현재보다 클 경우 - end를 현재로
+        if (start < current && current < end && is_history) {
+          return [...acc, { ...record, end: current }];
+        }
+
+        // schedule의 start값이 현재보다 작을 경우 - start를 현재로
+        if (current < end && start < current && !is_history) {
+          return [...acc, { ...record, start: current }];
+        }
+
+        // history가 현재 시각 이전의 구간일 경우 포함
+        if (end <= current && is_history) {
+          return [...acc, record];
+        }
+
+        // schedule이 현재 시각 이후의 구간일 경우 포함
+        if (current < start && !is_history) {
+          return [...acc, record];
+        }
+
+        return acc;
+      }, [] as RecordType[])
+      .sort((a, b) => a.start - b.start);
+  },
 });
