eeb114cd2da72b3b6186baa81d2f8b1f1aced51b
lynj <82319291+earlyoonj@users.noreply.github.com>
2023-09-17 12:26:10 +0900

    Feat: Player / Submit history #60

--- src/screens/Player.tsx
+++ src/screens/Player.tsx
@@ -1,12 +1,17 @@
-import { useRecoilValue } from 'recoil';
+import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
+import { useSubmit } from 'react-router-dom';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { styled } from 'styled-components';
 import { todoAtom } from '../atoms/todoAtom';
+import { isPlayingAtom } from '../atoms/uiAtom';
 import CheckButton from '../components/UI/button/CheckButton';
 import NavButton from '../components/UI/button/NavButton';
 import IconImageHolder from '../components/UI/general/IconImageHolder';
 import MainSubTitle from '../components/UI/general/MainSubTitle';
 import Timer from '../components/UI/general/Timer';
 import ProgressBar from '../components/UI/graph/ProgressBar';
+import { fetchRequest } from '../util/request';
 import { formatTime } from '../util/time';
 
 const PlayerWrapper = styled.div`
@@ -26,6 +31,8 @@ const PlayerWrapper = styled.div`
 `;
 
 const Player = () => {
+  const submit = useSubmit();
+
   const {
     daily_todo_id,
     category_name,
@@ -36,6 +43,39 @@ const Player = () => {
     schedule_sum,
     history_sum,
   } = useRecoilValue(todoAtom);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingAtom);
+
+  const [startDate, setStartDate] = useState(new Date());
+
+  useEffect(() => {
+    if (isPlaying) {
+      setStartDate(new Date());
+      if (!daily_todo_id) {
+        // TODO: 재생할 투두가 없습니다. -> slideMain false로
+        alert('시간 추적을 시작할 할 일이 없습니다.');
+        setIsPlaying(false);
+      }
+    } else {
+      const endDate = new Date();
+
+      if (!daily_todo_id) {
+        return;
+      }
+
+      if (endDate.getTime() - startDate.getTime() < 1000) {
+        return;
+      }
+
+      fetchRequest({
+        url: `/api/histories/daily-todos/${daily_todo_id}`,
+        method: 'post',
+        body: {
+          startDate: format(startDate, 'yyyy-MM-dd HH:mm:ss'),
+          endDate: format(endDate, 'yyyy-MM-dd HH:mm:ss'),
+        },
+      });
+    }
+  }, [isPlaying]);
 
   return (
     <PlayerWrapper className="w-80 mx-auto flex-column i-center gap-lg">
