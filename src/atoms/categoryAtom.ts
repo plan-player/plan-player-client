a439844f2355feb0723795f6c62ccc7c895cd247
gibeom <101088024+kkb4363@users.noreply.github.com>
2023-09-17 18:50:25 +0900

    ff

--- src/atoms/categoryAtom.ts
+++ src/atoms/categoryAtom.ts
@@ -1,21 +1,4 @@
-import { atom, selector } from 'recoil';
-import { fetchRequest } from '../util/request';
-import { CategoryGroupsProps } from '../components/Category/CategoryComponents';
-
-// export const categoryGroupAtom = selector({
-//   key: 'categoryGroup',
-//   get: async () => {
-//     try {
-//       const response = await fetchRequest({
-//         url: '/api/category-groups/all',
-//         method: 'GET',
-//       });
-//       return response as CategoryGroupsProps;
-//     } catch {
-//       return null;
-//     }
-//   },
-// });
+import { atom } from 'recoil';
 
 export const currentCategoryGroupAtom = atom({
   key: 'currentCategoryGroupAtom',
